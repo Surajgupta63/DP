a8a78c973540f4184605bb51ea3d03ce8a239737
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-19 18:48:25 +0530

    Update 198 House Robber.cpp

--- 198 House Robber.cpp
+++ 198 House Robber.cpp
@@ -31,3 +31,29 @@ public:
         return solve(nums.size()-1, nums, dp);
     }
 };
+
+// Approach--2 DP+Tabulation
+class Solution {
+public:
+    
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        vector<int> dp(nums.size()+1, 0);
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n; i++){
+            int pick    = nums[i-1] + dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick, notPick);
+        }
+        
+        return dp[n];
+
+
+    }
+};
