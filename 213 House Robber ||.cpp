50382cdfe1d5fe412f7a7be8dd6336d592f10820
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-19 19:53:06 +0530

    Update 213 House Robber ||.cpp

--- 213 House Robber ||.cpp
+++ 213 House Robber ||.cpp
@@ -25,3 +25,43 @@ public:
         return max(firstPart, secondPart);
     }
 };
+
+
+// Approach--1 DP+Tabualtion
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n==1) return nums[0];
+
+        int dp[101];
+
+        memset(dp, -1, sizeof(dp));
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n-1; i++){
+            int pick = nums[i-1] + dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick, notPick); 
+        }
+
+        int firstPart = dp[n-1];
+   
+        memset(dp, -1, sizeof(dp));
+        dp[1] = 0;
+        dp[2] = nums[1];
+
+        for(int i=3; i<=n; i++){
+            int pick = nums[i-1] + dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick, notPick); 
+        }
+
+        int secondPart = dp[n];
+
+        return max(firstPart, secondPart);
+    }
+};
