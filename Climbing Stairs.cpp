ec4add6826c909c945cb386f6994844d99bd384e
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-19 18:11:03 +0530

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -52,3 +52,24 @@ public:
     }
 };
 
+// Approach--3 Space Optimaztion
+
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        
+        if(n < 3) return n;
+
+        int a = 1;
+        int b = 2;
+        for(int i=3; i<=n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+};
+
