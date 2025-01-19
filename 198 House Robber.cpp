3e78901de522ee97b4d940a3048423b36c456246
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-19 18:25:55 +0530

    Update 198 House Robber.cpp

--- 198 House Robber.cpp
+++ 198 House Robber.cpp
@@ -13,3 +13,21 @@ public:
         return solve(0, nums);
     }
 };
+
+// Approach--1 DP+Memoization
+class Solution {
+public:
+    int solve(int i, vector<int>& nums, vector<int> &dp){
+        if(i >= nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick    = nums[i] + solve(i+2, nums, dp);
+        int notPick = solve(i+1, nums, dp);
+
+        return dp[i] = max(pick, notPick);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1, -1);
+        return solve(0, nums, dp);
+    }
+};
