74fad46bfe56a3a0d12497b7530697fdd07392a0
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-20 22:31:11 +0530

    Update 1911. Maximum Alternating Subsequence Sum.cpp

--- 1911. Maximum Alternating Subsequence Sum.cpp
+++ 1911. Maximum Alternating Subsequence Sum.cpp
@@ -1,4 +1,4 @@
-// Recursion + Memoization
+// Approach--1 Recursion + Memoization
 class Solution {
 public:
     long long dp[100001][2];
@@ -20,5 +20,22 @@ public:
     long long maxAlternatingSum(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));
         return solve(0, true, nums);
+    }
+};
+
+// Approach--2 Tabualtion
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<long long>> dp(n+1, vector<long long>(2, 0));
+
+        for(int i=1; i<=n; i++){
+            dp[i][0] = max(dp[i-1][1] - nums[i-1], dp[i-1][0]);
+            dp[i][1] = max(dp[i-1][0] + nums[i-1], dp[i-1][1]);
+        }
+
+        return max(dp[n][0], dp[n][1]);
     }
 };
