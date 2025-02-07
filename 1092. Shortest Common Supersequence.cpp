1f975cc62a5a8face2e11258aa57e8c56b90a428
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-02-07 21:57:41 +0530

    Create 1092. Shortest Common Supersequence.cpp

---
+++ 1092. Shortest Common Supersequence.cpp
@@ -0,0 +1,59 @@
+// Using Tabualtion
+// T.C : O(M*N)
+// S.C : O(M*N)
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.length();
+        int n = str2.length();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = i+j;
+                }
+                else if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        string ans = "";
+        int i = m, j = n;
+
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans += str1[i-1];
+                i--; j--;
+            }
+            else if(dp[i-1][j] < dp[i][j-1]){
+                ans += str1[i-1];
+                i--;
+            }
+            else{
+                ans += str2[j-1];
+                j--;
+            }
+
+        }
+
+        while(i > 0){
+            ans += str1[i-1];
+            i--;
+        }
+        while(j > 0){
+            ans += str2[j-1];
+            j--;
+        }
+
+        reverse(begin(ans), end(ans));
+
+        return ans;
+
+    }
+};
