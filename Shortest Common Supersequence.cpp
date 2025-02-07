2d875b75d3429a36b5f0819bceafeac60db6330c
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-02-07 21:30:00 +0530

    Update Shortest Common Supersequence.cpp

--- Shortest Common Supersequence.cpp
+++ Shortest Common Supersequence.cpp
@@ -52,3 +52,37 @@ class Solution {
         return dp[m][n];
     }
 };
+
+// Using Tabulation Of LCS(Longest Increasing Subsequence)
+// T.C : O(M*N)
+// S.C : O(M*N)
+class Solution {
+  public:
+    int shortestCommonSupersequence(string &s1, string &s2) {
+        int m = s1.length();
+        int n = s2.length();
+        
+        int dp[m+1][n+1];
+        
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        int lcs = dp[m][n];
+        
+        int rem_char_s1 = m - lcs;
+        int rem_char_s2 = n - lcs;
+        
+        return lcs + rem_char_s1 + rem_char_s2;
+    }
+};
